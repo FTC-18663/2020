d2727240d429240cef90fcb203db4e2a68e01b6d
aaronphal <aaronphal@gmail.com>
2021-03-20 11:01:01 -0500

    servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sweep.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Sweep.java
@@ -5,28 +5,29 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class Sweep {
 
     public Sweep() {
-        Robot.robotMap.sweep0.setDirection(Servo.Direction.FORWARD);
-        Robot.robotMap.sweep1.setDirection(Servo.Direction.FORWARD);
+        Robot.robotMap.stage0.setDirection(Servo.Direction.FORWARD);
+        Robot.robotMap.stage1.setDirection(Servo.Direction.FORWARD);
+        ///Robot.robotMap.stage0.scaleRange(0,180);
     }
 
     public void sweepMove(double position) {
 
         if (position > -30) {
-            Robot.robotMap.sweep0.setPosition(0.5);
-            Robot.robotMap.sweep1.setPosition(0.5);
+            Robot.robotMap.stage0.setPosition(0.5);
+            Robot.robotMap.stage1.setPosition(0.5);
         } else {
-            Robot.robotMap.sweep0.setPosition(0.1);
-            Robot.robotMap.sweep1.setPosition(0.1);
+            Robot.robotMap.stage0.setPosition(0.1);
+            Robot.robotMap.stage1.setPosition(0.1);
         }
     }
 
     public void sweeper(boolean button) {
         if(button) {
-            Robot.robotMap.sweep0.setPosition(1.0);
-            Robot.robotMap.sweep1.setPosition(1.0);
+            Robot.robotMap.stage0.setPosition(1.0);
+            Robot.robotMap.stage1.setPosition(1.0);
         } else {
-            Robot.robotMap.sweep0.setPosition(0.0);
-            Robot.robotMap.sweep1.setPosition(0.0);
+            Robot.robotMap.stage0.setPosition(0.0);
+            Robot.robotMap.stage1.setPosition(0.0);
         }
     }
 }
